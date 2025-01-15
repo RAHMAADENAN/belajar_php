0e1456d1243f03c55e28fc209323e93f165346b4
RAHMAADENAN <rahmaadenan561@gmail.com>
2025-01-15 16:14:38 +0700

    done

--- index.php
+++ index.php
@@ -7,9 +7,30 @@
 </head>
 <body>
     <?php
-    echo "rahma lope nuril, vanel lope abie.";
-    
-    echo " rahma jodoh abe di masa depan";
-    ?>
+    define("gaji", 5000000);
+    define("bonus", 500000);
+    define("pajak", 0.12);
+
+    $proyek = 2;
+    //$total_gaji;
+    //$gaji_bersih;
+
+    $proyek *= bonus;
+    $total_gaji = gaji + $proyek;
+    $gaji_bersih = $total_gaji - ($total_gaji * pajak);
+
+
+
+
+    echo "ini bonus anda $proyek <br/>";
+    echo "ini gaji anda setelah ditambah $total_gaji <br/>";
+    echo "ini gaji bersih anda $gaji_bersih";
+
+ 
+ 
+
+ 
+?>
 </body>
 </html>
+
